96fbce2fcf26866a0678461ee175976d9ab3278c
mohit <mohit@new_hp.com>
2016-06-26 12:40:49 +0530

    occurances

--- src/contacts.erl
+++ src/contacts.erl
@@ -76,10 +76,33 @@ my_search(Q1,[]) ->
   ok;
 
 my_search(Q1, [H1 | T1]) ->
-  Index = string:str(H1, Q1),
+  % Index = string:str(H1, Q1),
+  % Lets count occurances
+  Index = match(H1, Q1, Q1, 0),
   % io:format("searching:  index:~s term:~s  index ~n", [integer_to_list(Index), H1]),
   if Index =/= 0 ->
-    io:format("~s~n", [H1]);
+    io:format("~s occurances:~p~n", [H1,Index]);
     true -> ok
   end,
   my_search(Q1,T1).
+
+% https://www.rosettacode.org/wiki/Count_occurrences_of_a_substring#Erlang
+%% String and Sub exhausted, count a match and present result
+match([], [], _OrigSub, Acc) ->
+  Acc+1;
+
+%% String exhausted, present result
+match([], _Sub, _OrigSub, Acc) ->
+  Acc;
+
+%% Sub exhausted, count a match
+match(String, [], Sub, Acc) ->
+  match(String, Sub, Sub, Acc+1);
+
+%% First character matches, advance
+match([X|MainTail], [X|SubTail], Sub, Acc) ->
+  match(MainTail, SubTail, Sub, Acc);
+
+%% First characters do not match. Keep scanning for sub in remainder of string
+match([_X|MainTail], [_Y|_SubTail], Sub, Acc)->
+  match(MainTail, Sub, Sub, Acc).
